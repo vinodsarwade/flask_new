3367e69c7967296d9f813ec560fd1fd06d1c1ff8
vinodsarwade <vinodsarwade7@gmail.com>
2024-12-15 23:36:39 +0530

    vinod

--- Flask/API_.py
+++ Flask/API_.py
@@ -1,4 +1,4 @@
-#API
+# working with API--json
 
 from flask import Flask, jsonify, request
 
@@ -16,16 +16,54 @@ def Home():
 
 #Get: retrive all the items
 @app.route('/items',methods=['GET'])
-def get_tems():
+def get_items():
     return jsonify(items)
 
 
 ##Get: Retrive specific items by id
-@app.route('/items/<int:item_id>',method=['GET'])
+@app.route('/items/<int:item_id>',methods=['GET'])
 def get_item(item_id):
-    item = next((item for item in items if item['id']==item_id))
+    item = next((item for item in items if item['id']==item_id),None)
+    if item is None:
+        return jsonify({'error':'item not found'})
+    
+    return jsonify(item)
 
 
+#add new item 
+@app.route('/items',methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({'error':'item not found'})
+    new_item={
+        'id': items[-1]['id']+1 if items else 1,
+        'name' : request.json['name'],
+        'description': request.json['description']
+    }
+    items.append(new_item)
+    return jsonify(new_item)
+
+
+#Put: Update an existing item
+@app.route('/items/<int:item_id>',methods=['PUT'])
+def update_item(item_id):
+        item = next((item for item in items if item['id']==item_id),None)
+        if item is None:
+            return jsonify({'error':'item not found'})
+        
+        item['name'] = request.json.get('name',item['name'])
+        item['description'] = request.json.get('description',item['description'])
+
+        return jsonify(item)
+
+
+#Delete: Delete an item
+@app.route('/items/<int:item_id>',methods=['DELETE'])
+def delete_item(item_id):
+    global items
+    items = [item for item in items if item['id'] != item_id]
+    return jsonify({'error':'Item deleted'})
+    
 
 
 if __name__ =="__main__":
