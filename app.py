810c19c9da5b6e9305c535ba79eea70560b56cdd
vinodsarwade <vinodsarwade7@gmail.com>
2025-01-13 22:54:16 +0530

    vinod

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ stores = [
         ] 
     }
 ]
-
 @app.route("/store")
 def get_store():
-    return stores 
+    return stores
+
 
 @app.route("/store",methods=['POST'])
 def create_store():
@@ -27,6 +27,35 @@ def create_store():
     new_store = {"name":data['name'], "items":[]}
     stores.append(new_store)
     return new_store,200
+
+
+@app.route("/store/<string:name>/item",methods=['POST'])
+def create_item(name):
+    data = request.get_json()    #json in data
+    for store_name in stores:
+        print(store_name['name'])
+        if store_name['name'] == name:
+            new_item_toAdd = {"name":data["name"], "price":data["price"]}
+            store_name['items'].append(new_item_toAdd)
+            return new_item_toAdd, 200       
+    return f"{name} store not found"
+
+
+@app.route("/store/<string:name>")
+def get_store_name(name):
+    for store in stores:
+        if store['name'] == name:
+            return store
+    return f"{name} not found"
+
+
+@app.route("/store/<string:name>/item",methods=['GET'])
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return {"items": store['items']}
+    return f"{name} not found",404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
